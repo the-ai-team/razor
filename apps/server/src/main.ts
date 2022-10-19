e70cdeb78638c9139b9f4d6ba63aa5ab17dfb905
supunTE <supunedirisuriya2002@gmail.com>
2022-10-19 12:52:25 +0530

    feat(server): initialize empty express app
    Initialize empty express app
    
    re #2

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+import express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
