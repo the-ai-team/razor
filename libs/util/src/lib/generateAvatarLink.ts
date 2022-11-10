43d90fd044bdb822984a5ca56ebcde8c12a81129
supunTE <supunedirisuriya2002@gmail.com>
2022-11-10 09:33:10 +0530

    feat(util): set functions to convert plyaer name to hex in generateAvatarLink
    re #5

--- libs/util/src/lib/generateAvatarLink.ts
+++ libs/util/src/lib/generateAvatarLink.ts
@@ -1,7 +1,16 @@
 export const generateAvatarLink = async (
   playerName: string,
 ): Promise<string> => {
-  const seed = playerName;
+  const seed = bytesToHex(stringToUTF8Bytes(playerName));
+
   const image = `https://avatars.dicebear.com/api/open-peeps/${seed}.svg`;
   return image;
 };
+
+function bytesToHex(bytes: Uint8Array) {
+  return Array.from(bytes, byte => byte.toString(16).padStart(2, '0')).join('');
+}
+
+function stringToUTF8Bytes(text: string) {
+  return new TextEncoder().encode(text);
+}
