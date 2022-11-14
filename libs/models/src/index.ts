0825be5e77b0ce45c7f656e3e397b0bcd67ebfe2
supunTE <supunedirisuriya2002@gmail.com>
2022-11-14 09:26:46 +0530

    feat(data models): export sockets data models

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -4,3 +4,8 @@ export * from './lib/state/tournament';
 export * from './lib/state/leaderboard';
 export * from './lib/state/state-model';
 export * from './lib/state/log-message';
+
+export * from './lib/sockets/player';
+export * from './lib/sockets/race';
+export * from './lib/sockets/tournament';
+export * from './lib/sockets/leaderboard';
