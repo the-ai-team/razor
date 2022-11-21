c1a72459a33026f9493f161c75bf39e1d07e7e13
supunTE <supunedirisuriya2002@gmail.com>
2022-11-21 14:12:33 +0530

    test(effects): complete test code coverage of effects in player
    re #7

--- libs/store/src/lib/effects/player.spec.ts
+++ libs/store/src/lib/effects/player.spec.ts
@@ -1,30 +1,30 @@
 import {
   AppIdNumberType,
-  AppPlayerLogId,
   AppPlayerState,
-  AppRaceId,
   AppStateModel,
   AppTournamentState,
 } from '@razor/models';
 import {
-  tournamentNotFound,
-  invalidPlayerNameLength,
+  mockPlayersModel,
+  mockRace,
+  mockTournament,
+  M_GENERAL_ID0,
+  M_PLAYER_AVATAR0,
+  M_PLAYER_ID0,
+  M_PLAYER_NAME0,
+  M_TOURNAMENT0,
+  M_TOURNAMENT_ID0,
+  M_TR0_RACE_ID0,
+} from '../mocks';
+import {
   invalidPlayerName,
+  invalidPlayerNameLength,
+  payloadNotProvided,
   playerNotFound,
   raceNotFound,
+  tournamentNotFound,
 } from '../raisers';
 import { initializeStore } from '../store';
-
-const mockTournamentId1 = 'T:testTOUR';
-const mockPlayerId1 = 'P:testPLAY';
-const mockPlayerId2 = 'P:testPLY2';
-const mockGenralId1 = 'testUID1';
-const mockAvatarLink1 = 'https://test.com/avatar.png';
-const mockAvatarLink2 = 'https://test.com/avatar2.png';
-const mockPlayerName1 = 'Player1';
-const mockPlayerName2 = 'Player2';
-const mockRaceId1: AppRaceId = 'T:testTOUR-R:001';
-const mockPlayerLogId1: AppPlayerLogId = 'T:testTOUR-R:001-P:testPLAY';
 
 const initialState: AppStateModel = {
   tournamentsModel: {},
@@ -37,18 +37,30 @@ const initialState: AppStateModel = {
 
 jest.mock('@razor/util', () => ({
   ...jest.requireActual('@razor/util'),
-  generateUid: jest.fn().mockImplementation((type: AppIdNumberType) => {
+  generateUid: jest.fn((type: AppIdNumberType) => {
     switch (type) {
       case AppIdNumberType.Tournament:
-        return mockTournamentId1;
+        return M_TOURNAMENT_ID0;
       case AppIdNumberType.Player:
-        return mockPlayerId1;
+        return M_PLAYER_ID0;
       case AppIdNumberType.General:
-        return mockGenralId1;
+        return M_GENERAL_ID0;
     }
   }),
-  generateAvatarLink: jest.fn(() => mockAvatarLink1),
+  generateAvatarLink: jest.fn(() => M_PLAYER_AVATAR0),
 }));
+
+// when(generateUid)
+//   .calledWith(AppIdNumberType.Tournament)
+//   .mockImplementation(async () => M_TOURNAMENT_ID1);
+// when(generateUid)
+//   .calledWith(AppIdNumberType.Player)
+//   .mockImplementation(async () => M_PLAYER_ID0);
+// when(generateUid)
+//   .calledWith(AppIdNumberType.General)
+//   .mockImplementation(async () => M_GENERAL_ID0);
+// (generateAvatarLink as jest.Mock).mockReturnValue(M_PLAYER_AVATAR0);
+
 jest.mock('../raisers', () => ({
   ...jest.requireActual('../raisers'),
   tournamentNotFound: jest.fn(),
@@ -56,6 +68,7 @@ jest.mock('../raisers', () => ({
   invalidPlayerName: jest.fn(),
   playerNotFound: jest.fn(),
   raceNotFound: jest.fn(),
+  payloadNotProvided: jest.fn(),
 }));
 
 afterEach(() => {
@@ -71,79 +84,60 @@ describe('[Effects] Player', () => {
 
       await store.dispatch.game.joinPlayer({
         tid: '',
-        playerName: 'Player1',
+        playerName: M_PLAYER_NAME0,
       });
-      const gameState = store.getState();
+      const storeState = store.getState();
 
       const expectedResult = {
         ...initialValues,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId1],
-            raceIds: [],
-            state: AppTournamentState.Lobby,
-          },
+          [M_TOURNAMENT_ID0]: mockTournament(M_TOURNAMENT_ID0, [0, 0], [0, 1]),
         },
-        playersModel: {
-          [mockPlayerId1]: {
-            avatarLink: mockAvatarLink1,
-            name: mockPlayerName1,
-            state: AppPlayerState.Idle,
-            tournamentId: mockTournamentId1,
-          },
-        },
+        playersModel: mockPlayersModel([0, 1]),
       };
-      expect(gameState).toEqual({ ...initialStoreState, game: expectedResult });
+      expect(storeState).toEqual({
+        ...initialStoreState,
+        game: expectedResult,
+      });
     });
 
     it('(with valid tournament id) => Add player to playersModel and existing tournamentsModel', async () => {
       const initialValues: AppStateModel = {
         ...initialState,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId2],
-            raceIds: [],
-            state: AppTournamentState.Lobby,
-          },
+          [M_TOURNAMENT_ID0]: mockTournament(M_TOURNAMENT_ID0, [0, 1], [1, 3]),
         },
-        playersModel: {
-          [mockPlayerId2]: {
-            avatarLink: mockAvatarLink2,
-            name: mockPlayerName2,
-            state: AppPlayerState.Idle,
-            tournamentId: mockTournamentId1,
-          },
+        playersModel: mockPlayersModel([1, 3], M_TOURNAMENT_ID0),
+        racesModel: {
+          [M_TR0_RACE_ID0]: mockRace([1, 3]),
         },
       };
       const store = initializeStore(initialValues);
       const initialStoreState = store.getState();
 
       await store.dispatch.game.joinPlayer({
-        tid: mockTournamentId1,
-        playerName: mockPlayerName1,
+        tid: M_TOURNAMENT_ID0,
+        playerName: M_PLAYER_NAME0,
       });
-      const gameState = store.getState();
+      const storeState = store.getState();
 
       const expectedResult: AppStateModel = {
         ...initialValues,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId2, mockPlayerId1],
-            raceIds: [],
-            state: AppTournamentState.Lobby,
+          [M_TOURNAMENT_ID0]: {
+            ...initialValues.tournamentsModel[M_TOURNAMENT_ID0],
+            playerIds: [
+              ...initialValues.tournamentsModel[M_TOURNAMENT_ID0].playerIds,
+              M_PLAYER_ID0,
+            ],
           },
         },
-        playersModel: {
-          ...initialValues.playersModel,
-          [mockPlayerId1]: {
-            avatarLink: mockAvatarLink1,
-            name: mockPlayerName1,
-            state: AppPlayerState.Idle,
-            tournamentId: mockTournamentId1,
-          },
-        },
+        playersModel: mockPlayersModel([0, 3], M_TOURNAMENT_ID0),
       };
-      expect(gameState).toEqual({ ...initialStoreState, game: expectedResult });
+      expect(storeState).toEqual({
+        ...initialStoreState,
+        game: expectedResult,
+      });
     });
 
     it('(with invalid tournament id) => Raise error', async () => {
@@ -152,7 +146,7 @@ describe('[Effects] Player', () => {
 
       await store.dispatch.game.joinPlayer({
         tid: 'thisIdDoesNotExist',
-        playerName: 'Player1',
+        playerName: M_PLAYER_NAME0,
       });
       expect(tournamentNotFound).toHaveBeenCalled();
     });
@@ -174,83 +168,62 @@ describe('[Effects] Player', () => {
 
       await store.dispatch.game.joinPlayer({
         tid: '',
-        playerName: 'th1sN@m3IsN0tV@lid',
+        playerName: 'N@m3IsN0tV@lid',
       });
       expect(invalidPlayerName).toHaveBeenCalled();
     });
+
+    it('(payload empty) => Raise error', async () => {
+      const initialValues = initialState;
+      const store = initializeStore(initialValues);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      await store.dispatch.game.joinPlayer({} as any);
+      expect(payloadNotProvided).toHaveBeenCalled();
+    });
   });
+
   describe('Player Leave', () => {
     it('(valid id) => Clear Player', async () => {
       const initialValues: AppStateModel = {
         ...initialState,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId1, mockPlayerId2],
-            raceIds: [],
-            state: AppTournamentState.Lobby,
-          },
+          [M_TOURNAMENT_ID0]: M_TOURNAMENT0,
         },
-        playersModel: {
-          [mockPlayerId1]: {
-            avatarLink: mockAvatarLink1,
-            name: mockPlayerName1,
-            state: AppPlayerState.Idle,
-            tournamentId: mockTournamentId1,
-          },
-          [mockPlayerId2]: {
-            avatarLink: mockAvatarLink2,
-            name: mockPlayerName2,
-            state: AppPlayerState.Idle,
-            tournamentId: mockTournamentId1,
-          },
+        playersModel: mockPlayersModel([0, 3], M_TOURNAMENT_ID0),
+        racesModel: {
+          [M_TR0_RACE_ID0]: mockRace([0, 1]),
         },
       };
       const store = initializeStore(initialValues);
       const initialStoreState = store.getState();
 
       await store.dispatch.game.clearPlayer({
-        playerId: mockPlayerId1,
+        playerId: M_PLAYER_ID0,
       });
-      const gameState = store.getState();
+      const storeState = store.getState();
 
       const expectedResult: AppStateModel = {
         ...initialValues,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId2],
-            raceIds: [],
-            state: AppTournamentState.Lobby,
-          },
+          [M_TOURNAMENT_ID0]: mockTournament(M_TOURNAMENT_ID0, [0, 1], [1, 3]),
         },
-        playersModel: {
-          [mockPlayerId2]: {
-            avatarLink: mockAvatarLink2,
-            name: mockPlayerName2,
-            state: AppPlayerState.Idle,
-            tournamentId: mockTournamentId1,
-          },
-        },
+        playersModel: mockPlayersModel([1, 3], M_TOURNAMENT_ID0),
       };
-      expect(gameState).toEqual({ ...initialStoreState, game: expectedResult });
+      expect(storeState).toEqual({
+        ...initialStoreState,
+        game: expectedResult,
+      });
     });
 
     it('(invalid id) => Raise error', async () => {
       const initialValues: AppStateModel = {
         ...initialState,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId1],
-            raceIds: [],
-            state: AppTournamentState.Lobby,
-          },
+          [M_TOURNAMENT_ID0]: M_TOURNAMENT0,
         },
-        playersModel: {
-          [mockPlayerId1]: {
-            avatarLink: mockAvatarLink1,
-            name: mockPlayerName1,
-            state: AppPlayerState.Idle,
-            tournamentId: mockTournamentId1,
-          },
+        playersModel: mockPlayersModel([0, 3], M_TOURNAMENT_ID0),
+        racesModel: {
+          [M_TR0_RACE_ID0]: mockRace([0, 3]),
         },
       };
       const store = initializeStore(initialValues);
@@ -259,6 +232,13 @@ describe('[Effects] Player', () => {
         playerId: 'P:thisIdDoesNotExist',
       });
       expect(playerNotFound).toHaveBeenCalled();
+    });
+    it('(payload empty) => Raise error', async () => {
+      const initialValues = initialState;
+      const store = initializeStore(initialValues);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      await store.dispatch.game.clearPlayer({} as any);
+      expect(payloadNotProvided).toHaveBeenCalled();
     });
   });
 });
@@ -269,62 +249,37 @@ describe('[Effects] Player Log', () => {
       const initialValues: AppStateModel = {
         ...initialState,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId1, mockPlayerId2],
-            raceIds: [mockRaceId1],
-            state: AppTournamentState.Lobby,
+          [M_TOURNAMENT_ID0]: {
+            ...M_TOURNAMENT0,
+            state: AppTournamentState.Race,
           },
         },
         racesModel: {
-          [mockRaceId1]: {
-            text: 'Mollit ea ea nisi occaecat ex. Esse pariatur excepteur irure eu aute magna amet non aliquip minim veniam sit. Cupidatat ex quis occaecat quis voluptate in aute.',
-            timeoutDuration: 120,
-            startedTimestamp: 123456789,
-            players: {
-              [mockPlayerId1]: {
-                avatarLink: mockAvatarLink1,
-                name: mockPlayerName1,
-              },
-              [mockPlayerId2]: {
-                avatarLink: mockAvatarLink2,
-                name: mockPlayerName2,
-              },
-            },
-            isOnGoing: true,
-            raceStartedBy: mockPlayerId1,
-          },
+          [M_TR0_RACE_ID0]: mockRace([0, 3], true),
         },
-        playersModel: {
-          [mockPlayerId1]: {
-            avatarLink: mockAvatarLink1,
-            name: mockPlayerName1,
-            state: AppPlayerState.Racing,
-            tournamentId: mockTournamentId1,
-          },
-          [mockPlayerId2]: {
-            avatarLink: mockAvatarLink2,
-            name: mockPlayerName2,
-            state: AppPlayerState.Racing,
-            tournamentId: mockTournamentId1,
-          },
-        },
+        playersModel: mockPlayersModel(
+          [0, 3],
+          M_TOURNAMENT_ID0,
+          AppPlayerState.Racing,
+        ),
       };
       const store = initializeStore(initialValues);
       const initialStoreState = store.getState();
 
       await store.dispatch.game.sendTypeLog({
-        playerId: mockPlayerId1,
-        raceId: mockRaceId1,
+        playerId: M_PLAYER_ID0,
+        raceId: M_TR0_RACE_ID0,
         playerLog: {
           textLength: 0,
           timestamp: 123456000,
         },
       });
+      const storeState = store.getState();
 
       const expectedResult: AppStateModel = {
         ...initialValues,
         playerLogsModel: {
-          [mockPlayerLogId1]: [
+          [`${M_TR0_RACE_ID0}-${M_PLAYER_ID0}`]: [
             {
               textLength: 0,
               timestamp: 123456000,
@@ -333,57 +288,31 @@ describe('[Effects] Player Log', () => {
         },
       };
 
-      const gameState = store.getState();
-      expect(gameState).toEqual({ ...initialStoreState, game: expectedResult });
+      expect(storeState).toEqual({
+        ...initialStoreState,
+        game: expectedResult,
+      });
     });
     it('(player not exist) => Raise error', async () => {
       const initialValues: AppStateModel = {
         ...initialState,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId1, mockPlayerId2],
-            raceIds: [mockRaceId1],
-            state: AppTournamentState.Race,
-          },
+          [M_TOURNAMENT_ID0]: M_TOURNAMENT0,
         },
-        playersModel: {
-          [mockPlayerId1]: {
-            avatarLink: mockAvatarLink1,
-            name: mockPlayerName1,
-            state: AppPlayerState.Racing,
-            tournamentId: mockTournamentId1,
-          },
-          [mockPlayerId2]: {
-            avatarLink: mockAvatarLink2,
-            name: mockPlayerName2,
-            state: AppPlayerState.Racing,
-            tournamentId: mockTournamentId1,
-          },
-        },
+        playersModel: mockPlayersModel(
+          [0, 3],
+          M_TOURNAMENT_ID0,
+          AppPlayerState.Racing,
+        ),
+
         racesModel: {
-          [mockRaceId1]: {
-            text: 'Mollit ea ea nisi occaecat ex. Esse pariatur excepteur irure eu aute magna amet non aliquip minim veniam sit. Cupidatat ex quis occaecat quis voluptate in aute.',
-            timeoutDuration: 120,
-            startedTimestamp: 123456789,
-            players: {
-              [mockPlayerId1]: {
-                avatarLink: mockAvatarLink1,
-                name: mockPlayerName1,
-              },
-              [mockPlayerId2]: {
-                avatarLink: mockAvatarLink2,
-                name: mockPlayerName2,
-              },
-            },
-            isOnGoing: true,
-            raceStartedBy: mockPlayerId1,
-          },
+          [M_TR0_RACE_ID0]: mockRace([0, 3], true),
         },
       };
       const store = initializeStore(initialValues);
 
       await store.dispatch.game.sendTypeLog({
-        raceId: mockRaceId1,
+        raceId: M_TR0_RACE_ID0,
         playerId: 'P:thisIdDoesNotExist',
         playerLog: {
           textLength: 0,
@@ -397,60 +326,54 @@ describe('[Effects] Player Log', () => {
       const initialValues: AppStateModel = {
         ...initialState,
         tournamentsModel: {
-          [mockTournamentId1]: {
-            playerIds: [mockPlayerId1, mockPlayerId2],
-            raceIds: [mockRaceId1],
-            state: AppTournamentState.Race,
-          },
+          [M_TR0_RACE_ID0]: M_TOURNAMENT0,
         },
-        playersModel: {
-          [mockPlayerId1]: {
-            avatarLink: mockAvatarLink1,
-            name: mockPlayerName1,
-            state: AppPlayerState.Racing,
-            tournamentId: mockTournamentId1,
-          },
-          [mockPlayerId2]: {
-            avatarLink: mockAvatarLink2,
-            name: mockPlayerName2,
-            state: AppPlayerState.Racing,
-            tournamentId: mockTournamentId1,
-          },
-        },
+        playersModel: mockPlayersModel(
+          [0, 3],
+          M_TOURNAMENT_ID0,
+          AppPlayerState.Racing,
+        ),
         racesModel: {
-          [mockRaceId1]: {
-            text: 'Mollit ea ea nisi occaecat ex. Esse pariatur excepteur irure eu aute magna amet non aliquip minim veniam sit. Cupidatat ex quis occaecat quis voluptate in aute.',
-            timeoutDuration: 120,
-            startedTimestamp: 123456789,
-            players: {
-              [mockPlayerId1]: {
-                avatarLink: mockAvatarLink1,
-                name: mockPlayerName1,
-              },
-              [mockPlayerId2]: {
-                avatarLink: mockAvatarLink2,
-                name: mockPlayerName2,
-              },
-            },
-            isOnGoing: true,
-            raceStartedBy: mockPlayerId1,
-          },
+          [M_TR0_RACE_ID0]: mockRace([0, 3], true),
         },
       };
       const store = initializeStore(initialValues);
 
       await store.dispatch.game.sendTypeLog({
-        raceId: 'T:testTOUR-R:notExist',
-        playerId: mockPlayerId1,
+        raceId: 'T:testTR00-R:notExist',
+        playerId: M_PLAYER_ID0,
         playerLog: {
           textLength: 0,
           timestamp: 123456000,
         },
       });
-      console.log(
-        store.getState().game.playerLogsModel['T:testTOUR-R:001-P:testPLAY'],
-      );
       expect(raceNotFound).toHaveBeenCalled();
+    });
+    it('(raceId payload empty) => Raise error', async () => {
+      const initialValues = initialState;
+      const store = initializeStore(initialValues);
+      await store.dispatch.game.sendTypeLog({
+        playerId: M_PLAYER_ID0,
+        playerLog: {
+          textLength: 0,
+          timestamp: 123456000,
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any);
+      expect(payloadNotProvided).toHaveBeenCalled();
+    });
+    it('(playerId payload empty) => Raise error', async () => {
+      const initialValues = initialState;
+      const store = initializeStore(initialValues);
+      await store.dispatch.game.sendTypeLog({
+        raceId: M_TR0_RACE_ID0,
+        playerLog: {
+          textLength: 0,
+          timestamp: 123456000,
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any);
+      expect(payloadNotProvided).toHaveBeenCalled();
     });
   });
 });
