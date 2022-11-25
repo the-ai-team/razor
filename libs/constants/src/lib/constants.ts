b541ee432f0aa171c94feb37d55fa7ba7b096ccc
supunTE <supunedirisuriya2002@gmail.com>
2022-11-25 12:08:41 +0530

    feat(constants): update constants

--- libs/constants/src/lib/constants.ts
+++ libs/constants/src/lib/constants.ts
@@ -1,1 +1,11 @@
 export const MAX_ERR_LOGS_COUNT = 1024;
+
+export const TOURNAMENT_ID_LENGTH = 8;
+export const PLAYER_ID_LENGTH = 8;
+export const GENERAL_ID_LENGTH = 8;
+export const RACE_ID_LENGTH = 3;
+
+export const NANOID_ALPHABET =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+export const AVERAGE_WPM = 50;
