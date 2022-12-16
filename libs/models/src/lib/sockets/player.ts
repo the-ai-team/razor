2c546fa9df8502966d757bac9e86a58d4bf218dc
supunTE <supunedirisuriya2002@gmail.com>
2022-12-16 19:18:59 +0530

    feat(sockets): add schema validation using zod
    add schema validation and refactor data models in socket
    
    re #17

--- libs/models/src/lib/sockets/player.ts
+++ libs/models/src/lib/sockets/player.ts
@@ -1,11 +1,17 @@
-/** Interface for time-logs when players are typing */
-interface TimeLog {
-  /** Correctly typed length by player */
-  textLength: number;
-  /** Time when player typed the last character */
-  timestamp: number;
-}
+import { PLAYER_NAME_RANGE } from '@razor/constants';
+import { z } from 'zod';
 
+// ==== Primary Schemas ==== //
+export const playerIdSchema = z.custom<`P:${string}`>(id =>
+  /^P:[a-zA-Z0-9]{8}$/.test(id as string),
+);
+export const playerNameSchema = z
+  .string()
+  .min(PLAYER_NAME_RANGE[0])
+  .max(PLAYER_NAME_RANGE[1])
+  .regex(/^[a-zA-Z0-9_]+$/);
+
+// ==== Enums ==== //
 export enum PlayerState {
   /** **Player idle**
    *
@@ -25,18 +31,19 @@ export enum PlayerState {
   Racing = 'racing',
 }
 
+// ==== Types ==== //
+/** Player id template literal */
+export type playerId = z.input<typeof playerIdSchema>;
+
+// ==== Interfaces ==== //
+// Note: `Player` does not need to be a schema; because it's only bound to the server-to-client communication.
 export interface Player {
   /** Unique player id */
-  id: string;
+  id: playerId;
   /** Player name */
   name: string;
   /** Player avatar icon URL */
   avatarLink: string;
   /** Player state */
   state: PlayerState;
-}
-
-export interface PlayerWithLogs extends Omit<Player, 'state' | 'lobbyId'> {
-  /** Array of time logs of player */
-  logs: TimeLog[];
 }
