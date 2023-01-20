5fb5d590b8f828bdcafa95a602273a685661db47
supunTE <supunedirisuriya2002@gmail.com>
2023-01-20 18:46:49 +0530

    refactor: change import to load from assets

--- apps/client/src/pages/layout.tsx
+++ apps/client/src/pages/layout.tsx
@@ -1,11 +1,11 @@
 import { ReactElement } from 'react';
 // Assets
-import logo from '../assets/images/logo.png';
+// import logo from '../assets/images/logo.png';
 
 export function Layout(): ReactElement {
   return (
     <div className='px-10 py-5 h-full'>
-      <img src={logo} alt='' />
+      <img src='./assets/images/logo.png' alt='' />
     </div>
   );
 }
