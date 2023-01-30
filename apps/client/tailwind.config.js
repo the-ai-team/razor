0c3b23e53ea9ce434027be082479d8f0d99526d5
supunTE <supunedirisuriya2002@gmail.com>
2023-01-30 12:07:56 +0530

    feat(tailwind): add theme colors

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,17 +1,20 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const colors = require('tailwindcss/colors')
 const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(
-      __dirname,
-      'src/**/*!(*.stories|*.spec).tsx',
-    ),
+    join(__dirname, 'src/**/*!(*.stories|*.spec).tsx'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    colors: {
+      text: {
+        light: '#F4DDDB',
+      },
+      white: colors.white,
+    },
   },
   plugins: [],
 };
