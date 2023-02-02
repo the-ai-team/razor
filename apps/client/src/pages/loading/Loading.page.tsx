c92a80e23f0e9581fb997bfae5e9e073687174ec
supunTE <supunedirisuriya2002@gmail.com>
2023-02-02 16:11:30 +0530

    refactor(client/laoding): change loading icon styles to tailwind classes

--- apps/client/src/pages/loading/Loading.page.tsx
+++ apps/client/src/pages/loading/Loading.page.tsx
@@ -5,7 +5,7 @@ export function Loading(): ReactElement {
   return (
     <div className='flex h-full'>
       <div className='m-auto flex gap-2'>
-        <img src={LoaderIcon} alt='' width='200px' height='200px' />
+        <img src={LoaderIcon} alt='' className='w-52 h-52' />
         <div className='text-5xl m-auto'>Loading</div>
       </div>
     </div>
