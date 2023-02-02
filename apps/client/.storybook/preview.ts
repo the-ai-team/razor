8e49f557a13544a1c3a204d1864120b1a937c61e
supunTE <supunedirisuriya2002@gmail.com>
2023-02-02 14:14:23 +0530

    build(storybook): add storybook addon plugin

--- apps/client/.storybook/preview.ts
+++ apps/client/.storybook/preview.ts
@@ -10,4 +10,13 @@ export const parameters = {
       },
     ],
   },
+  paddings: {
+    values: [
+      { name: 'Small', value: '16px' },
+      { name: 'Medium', value: '64px' },
+      { name: 'Large', value: '128px' },
+      { name: 'Extra Large', value: '256px' },
+    ],
+    default: 'Medium',
+  },
 };
