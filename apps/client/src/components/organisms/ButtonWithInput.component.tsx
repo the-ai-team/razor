a674b579a1ece22fdb4be704667f2afe3dc973de
supunTE <supunedirisuriya2002@gmail.com>
2023-02-06 10:19:09 +0530

    feat(client/components): create button with input component
    re #33

---
+++ apps/client/src/components/organisms/ButtonWithInput.component.tsx
@@ -0,0 +1,53 @@
+import { ReactElement, useState } from 'react';
+import { Button, Input } from '../molecules';
+
+export interface ButtonWithInputProps {
+  onClick: (value: string, e: React.MouseEvent<HTMLButtonElement>) => void;
+  isDisable?: boolean;
+  children: string;
+  inputPlaceholder?: string;
+  inputSize?: number;
+  maxInputLength?: number;
+}
+
+export function ButtonWithInput({
+  onClick,
+  isDisable,
+  children,
+  inputPlaceholder,
+  inputSize,
+  maxInputLength,
+}: ButtonWithInputProps): ReactElement {
+  inputSize ||= 12;
+  maxInputLength ||= 12;
+
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.target.value);
+  };
+
+  const buttonClickHandler = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    // Prevents onClick from firing when input element is clicked
+    if (!(e.target instanceof HTMLInputElement)) {
+      onClick(inputValue, e);
+    }
+  };
+
+  return (
+    <Button
+      isDisable={isDisable}
+      onClick={buttonClickHandler}
+      input={
+        <div>
+          <Input
+            onChange={inputChangeHandler}
+            placeholder={inputPlaceholder}
+            props={{ size: inputSize, maxLength: maxInputLength }}
+          />
+        </div>
+      }>
+      {children}
+    </Button>
+  );
+}
