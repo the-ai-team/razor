2ad961d797b1b7b2af30be8ab51e782bb4e848b3
supunTE <supunedirisuriya2002@gmail.com>
2023-02-15 12:49:44 +0530

    feat(server): setup socket-io on server
    re #48

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,33 @@
-import express = require('express');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+const allowedOrigin =
+  process.env.NX_ALLOWED_ORIGINS.split(', ') || 'http://localhost:4200';
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigin,
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}`);
 });
