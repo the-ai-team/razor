89607238247c62e75c67ead23c709f67b154a03f
supunTE <supunedirisuriya2002@gmail.com>
2023-03-29 17:50:51 +0530

    refactor(client/race): remove some unwanted code

--- apps/client/src/pages/race/templates/RaceTrack.template.tsx
+++ apps/client/src/pages/race/templates/RaceTrack.template.tsx
@@ -1,12 +1,10 @@
-import { ReactElement, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { M_TR0_RACE_ID0 } from '@razor/mocks';
-import { AppPlayerId, AppRaceId } from '@razor/models';
-import { Dispatch, RootState } from '@razor/store';
+import { AppRaceId } from '@razor/models';
+import { RootState } from '@razor/store';
 import { extractId, ExtractIdType } from '@razor/util';
+import { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
 
 import { RaceBackground } from './race-background';
-import { RaceLine } from './RaceLine.template';
 
 import cs from 'classnames';
 
@@ -16,23 +14,6 @@ interface RaceTrackProps {
 
 export function RaceTrack({ raceId }: RaceTrackProps): ReactElement {
   const game = useSelector((store: RootState) => store.game);
-  const dispatch: Dispatch = useDispatch();
-
-  const updateHandler = useCallback(
-    (playerId: AppPlayerId): void => {
-      const playerLog = game.playerLogsModel[`${raceId}-${playerId}`];
-      const lastPlayerLog = playerLog[playerLog.length - 1];
-      dispatch.game.sendTypeLog({
-        raceId: M_TR0_RACE_ID0,
-        playerId,
-        playerLog: {
-          timestamp: Date.now(),
-          textLength: lastPlayerLog.textLength + 10,
-        },
-      });
-    },
-    [dispatch.game, game.playerLogsModel, raceId],
-  );
 
   const tournamentId = extractId(
     raceId,
@@ -40,7 +21,6 @@ export function RaceTrack({ raceId }: RaceTrackProps): ReactElement {
     ExtractIdType.Tournament,
   );
   const playerIds = game.tournamentsModel[tournamentId].playerIds;
-  const textLength = game.racesModel[raceId].text.length;
 
   return (
     <div>
@@ -48,23 +28,6 @@ export function RaceTrack({ raceId }: RaceTrackProps): ReactElement {
         count={playerIds.length}
         className={cs('mx-auto my-10')}
       />
-      {playerIds.map((playerId: AppPlayerId) => {
-        return (
-          <div key={playerId}>
-            <RaceLine
-              raceId={raceId}
-              playerId={playerId}
-              raceTextLength={textLength}
-            />
-            <button
-              type='button'
-              className='bg-neutral-40 p-2'
-              onClick={(): void => updateHandler(playerId)}>
-              +
-            </button>
-          </div>
-        );
-      })}
     </div>
   );
 }
