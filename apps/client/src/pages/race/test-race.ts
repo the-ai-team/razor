4b94cfb1766010871c3e66899c90d47ebdd855e2
supunTE <supunedirisuriya2002@gmail.com>
2023-03-29 17:53:17 +0530

    refactor(client/race): use mock racing text instead of calling api

--- apps/client/src/pages/race/test-race.ts
+++ apps/client/src/pages/race/test-race.ts
@@ -1,15 +1,17 @@
+import { M_RACE_TEXT0 } from '@razor/mocks';
 import { AppPlayerId } from '@razor/models';
 import { store } from '@razor/store';
 
-const loadRacingText = async (): Promise<string> => {
-  const url = 'http://www.metaphorpsum.com/paragraphs/1/8';
+// TODO: Should be moved to server
+// const loadRacingText = async (): Promise<string> => {
+//   const url = 'https://www.metaphorpsum.com/paragraphs/1/8';
 
-  return await fetch(url)
-    .then(response => response.text())
-    .then(data => {
-      return data;
-    });
-};
+//   return await fetch(url)
+//     .then(response => response.text())
+//     .then(data => {
+//       return data;
+//     });
+// };
 
 store.dispatch.game.joinPlayer({
   receivedTournamentId: '',
@@ -27,29 +29,27 @@ store.dispatch.game.joinPlayer({
   playerName: 'Player2',
 });
 
-(async (): Promise<void> => {
-  store.dispatch.game.startCountdown({
-    tournamentId: tournamentId,
-    playerId: player1Id,
-    raceText: await loadRacingText(),
+store.dispatch.game.startCountdown({
+  tournamentId: tournamentId,
+  playerId: player1Id,
+  raceText: M_RACE_TEXT0,
+});
+
+game = store.getState().game;
+const playerIds = game.tournamentsModel[tournamentId].playerIds;
+
+playerIds.forEach(playerId => {
+  store.dispatch.game.sendTypeLog({
+    raceId: `${tournamentId}-R:000`,
+    playerId: playerId,
+    playerLog: {
+      timestamp: Date.now(),
+      textLength: 0,
+    },
   });
+});
 
-  game = store.getState().game;
-  const playerIds = game.tournamentsModel[tournamentId].playerIds;
-
-  playerIds.forEach(playerId => {
-    store.dispatch.game.sendTypeLog({
-      raceId: `${tournamentId}-R:000`,
-      playerId: playerId,
-      playerLog: {
-        timestamp: Date.now(),
-        textLength: 0,
-      },
-    });
-  });
-
-  game = store.getState().game;
-})();
+game = store.getState().game;
 
 export const addPlayer = (count: number): void => {
   console.log('addPlayer', count);
