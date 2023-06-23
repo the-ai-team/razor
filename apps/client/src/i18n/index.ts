06a06e7bc3e3885b1e768b0c48e2b727c1fe797a
supunTE <supunedirisuriya2002@gmail.com>
2023-06-23 13:13:22 +0530

    feat(i18n): add translations to room.page

--- apps/client/src/i18n/index.ts
+++ apps/client/src/i18n/index.ts
@@ -1,18 +1,20 @@
 import { initReactI18next } from 'react-i18next';
 import i18next from 'i18next';
 
-import home from './en/home.json';
+import home_en from './en/home.json';
+import room_en from './en/room.json';
 
 export const resources = {
   en: {
-    home,
+    home: home_en,
+    room: room_en,
   },
 };
 
 i18next.use(initReactI18next).init({
   lng: 'en',
   resources,
-  ns: ['home'],
+  ns: ['home', 'room'],
   interpolation: {
     escapeValue: false,
   },
