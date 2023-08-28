0ade850db4003c1e772146a1642abba89c5c58f3
supunTE <supunedirisuriya2002@gmail.com>
2023-08-28 17:54:52 +0530

    feat(components): develop cursor component

---
+++ apps/client/src/components/atoms/cursor/Cursor.component.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from 'react';
+import cs from 'classnames';
+
+export interface CursorProps {
+  isDebug?: boolean;
+  isAtSpace?: boolean;
+}
+
+export function Cursor({
+  isDebug = false,
+  isAtSpace = false,
+}: CursorProps): ReactElement {
+  return (
+    <span
+      className={cs(
+        'absolute h-full w-[20px] top-[0.5px]',
+        {
+          'left-[-0.025em]': !isAtSpace,
+          'bg-primary-30 bg-opacity-60': isDebug,
+        },
+        'animate-cursor-blink',
+      )}>
+      <div className={cs('bg-neutral-90', 'h-full w-[2.5px] rounded-full')} />
+    </span>
+  );
+}
