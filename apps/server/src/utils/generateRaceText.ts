f4ccc3d7187d9cf2c0ec49148453cfe0e0b0aaa8
supunTE <supunedirisuriya2002@gmail.com>
2023-10-03 12:12:09 +0530

    feat(utils): add regex to generate race text function to filter unnecessary char types

--- apps/server/src/utils/generateRaceText.ts
+++ apps/server/src/utils/generateRaceText.ts
@@ -7,6 +7,11 @@ export const generateRaceText = async (): Promise<string> => {
   try {
     const response = await axios.get(url);
     raceText = response.data;
+
+    // Drop any char that is not a letter, space, comma, period, apostrophe, question mark, or exclamation point.
+    // (Also drop new line)
+    raceText = raceText.replace(/[^a-zA-Z ,.'?!]/g, '');
+
     return raceText;
   } catch (e) {
     throw new Error(e);
