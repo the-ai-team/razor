42fc9caf11b084fa0a394bd3c746e92a49772ed6
supunTE <supunedirisuriya2002@gmail.com>
2023-10-03 10:19:46 +0530

    feat(translations): add common translations file

--- apps/client/src/i18n/index.ts
+++ apps/client/src/i18n/index.ts
@@ -2,19 +2,21 @@ import { initReactI18next } from 'react-i18next';
 import i18next from 'i18next';
 
 import home_en from './en/home.json';
+import race_en from './en/race.json';
 import room_en from './en/room.json';
 
 export const resources = {
   en: {
     home: home_en,
     room: room_en,
+    race: race_en,
   },
 };
 
 i18next.use(initReactI18next).init({
   lng: 'en',
   resources,
-  ns: ['home', 'room'],
+  ns: ['home', 'room', 'race', 'common'],
   interpolation: {
     escapeValue: false,
   },
