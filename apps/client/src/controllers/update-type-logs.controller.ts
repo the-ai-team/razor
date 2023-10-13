a76717eed311943cc36e223fbecc101ecea872b4
supunTE <supunedirisuriya2002@gmail.com>
2023-10-13 16:26:13 +0530

    feat(client/controllers): develop controller for update type logs

---
+++ apps/client/src/controllers/update-type-logs.controller.ts
@@ -0,0 +1,32 @@
+import { PlayerId, socketProtocols } from '@razor/models';
+import { store } from '@razor/store';
+
+import { AllServerPubSubEventsToTypeMap } from '../models';
+import { pubsub } from '../utils/pubsub';
+
+type UpdateTypeLogsControllerArgs =
+  AllServerPubSubEventsToTypeMap[socketProtocols.UpdateTypeLogs];
+
+function updateTypeLogsController({
+  data,
+  savedPlayerId,
+}: UpdateTypeLogsControllerArgs): void {
+  const { raceId, playerLogs: playersLogs } = data;
+  const race = store.getState().game.racesModel[raceId];
+  const racePlayerIds = Object.keys(race.players) as PlayerId[];
+
+  for (const playerId of racePlayerIds) {
+    const playerLogs = playersLogs[playerId];
+    if (!playerLogs || playerId === savedPlayerId) {
+      continue;
+    }
+
+    store.dispatch.game.sendTypeLog({
+      raceId: raceId,
+      playerId,
+      playerLog: playerLogs,
+    });
+  }
+}
+
+pubsub.subscribe(socketProtocols.UpdateTypeLogs, updateTypeLogsController);
