d0de22838f0d6329981f65eedfa2775d45d70581
supunTE <supunedirisuriya2002@gmail.com>
2023-10-16 15:58:25 +0530

    fix(client/controllers): export missing controller

--- apps/client/src/controllers/index.ts
+++ apps/client/src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './create-tournament.controller';
 export * from './join-tournament.controller';
 export * from './player-join.controller';
 export * from './start-race.controller';
+export * from './update-type-logs.controller';
