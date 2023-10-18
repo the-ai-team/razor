04665d3e9d8bb96dcf81075c10d64e1074fa5a71
supunTE <supunedirisuriya2002@gmail.com>
2023-10-18 22:11:50 +0530

    fix(client/race): fix key press listerner not detecting other keyboard layouts

--- apps/client/src/pages/race/templates/race-text/utils/key-press-listener.ts
+++ apps/client/src/pages/race/templates/race-text/utils/key-press-listener.ts
@@ -10,8 +10,6 @@ export function useKeyPress(
 
     const simpleLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
     const capitalLetters = simpleLetters.map(letter => letter.toUpperCase());
-    // Keycodes: KeyA, KeyB,...
-    const keyCodes = capitalLetters.map(letter => `Key${letter}`);
 
     const handleKeyPress = (e: KeyboardEvent): void => {
       // If event target on a input, don't do anything; else, prevent default
@@ -19,48 +17,43 @@ export function useKeyPress(
       if (e.target instanceof HTMLInputElement) {
         return;
       }
+
+      if (e.ctrlKey) {
+        return;
+      }
       e.preventDefault();
 
-      if (e.code === 'Space') {
+      if (e.key === ' ') {
         return inputHandler('SPACE');
       }
-      if (e.code === 'Backspace') {
+      if (e.key === 'Backspace') {
         return inputHandler('BACKSPACE');
       }
 
       //   Special chars
-      if (e.code === 'Comma') {
+      if (e.key === ',') {
         return inputHandler(',');
       }
-
-      if (e.code === 'Period') {
+      if (e.key === '.') {
         return inputHandler('.');
       }
-
-      if (e.code === 'Quote') {
+      if (e.key === "'") {
         return inputHandler("'");
       }
-
       if (e.key === '?') {
         return inputHandler('?');
       }
-
       if (e.key === '!') {
         return inputHandler('!');
       }
 
-      // t f =>  t
-      // f t =>  t
-      // f f =>  f
-      // t t =>  f
-      // Not equal act as XOR here
-      const isCapital =
-        e.getModifierState('CapsLock') !== e.getModifierState('Shift');
-
-      // If key code between 65 and 90, it's a letter, input correct letter based on caps lock
-      for (let i = 0; i < keyCodes.length; i++) {
-        if (e.code === keyCodes[i]) {
-          return inputHandler(isCapital ? capitalLetters[i] : simpleLetters[i]);
+      // If key is a letter, return the letter
+      for (let i = 0; i < simpleLetters.length; i++) {
+        if (e.key === simpleLetters[i]) {
+          return inputHandler(simpleLetters[i]);
+        }
+        if (e.key === capitalLetters[i]) {
+          return inputHandler(capitalLetters[i]);
         }
       }
     };
